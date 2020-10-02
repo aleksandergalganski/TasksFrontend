3fe608b2326055881923c7ca24c7666a0453a06b
olekgalgan <olekgalgan@gmail.com>
2020-10-02 12:16:15 +0200

    Update Navbar component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { logout } from '../../actions/authActions';
+import { clearTasks } from '../../actions/tasksActions';
+// Material UI
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
-const Navbar = () => {
-  const isAuthenticated = false;
+const Navbar = ({ auth: { isAuthenticated }, logout, clearTasks }) => {
+  const onLogout = () => {
+    logout();
+    clearTasks();
+  };
 
   return (
     <nav style={{ marginBottom: '80px' }}>
@@ -15,18 +24,21 @@ const Navbar = () => {
           <Typography variant='h6'>Tasks App</Typography>
           <div className='toolbar__buttons'>
             {isAuthenticated ? (
-              <React.Fragment>
-                <Button color='inherit'>Logout</Button>
-              </React.Fragment>
+              <Fragment>
+                <AccountCircle />
+                <Button color='inherit' onClick={onLogout}>
+                  Logout
+                </Button>
+              </Fragment>
             ) : (
-              <React.Fragment>
+              <Fragment>
                 <Button color='primary'>
                   <Link to='/login'>Login</Link>
                 </Button>
                 <Button color='primary'>
                   <Link to='/register'>Register</Link>
                 </Button>
-              </React.Fragment>
+              </Fragment>
             )}
           </div>
         </ToolBar>
@@ -35,4 +47,13 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+Navbar.propTypes = {
+  auth: PropTypes.object.isRequired,
+  logout: PropTypes.func.isRequired
+};
+
+export default connect(mapStateToProps, { logout, clearTasks })(Navbar);
